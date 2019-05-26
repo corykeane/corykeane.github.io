b1cbb033e69d7288108607b7be4ac5f489ed0efb
Cory <cory@Corys-Mac-mini.attlocal.net>
2019-05-25 20:35:05 -0500

    r1d5 exersies

---
+++ exercises/main.js
@@ -0,0 +1,3 @@
+TweenMax.to(".logo", 3, {rotation: 360, scale:0.5, ease: Elastic.easeOut});
+TweenMax.from(".text", 2, {opacity: 0, y: 50, delay: 1});
+TweenMax.from(".button", 0.5, {opacity: 0, delay: 3});
